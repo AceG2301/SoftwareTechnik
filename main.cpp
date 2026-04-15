2fd9b00ff6e59fca4c658a9a2935b303c5cddc27
Rey <treyindra1@gmail.com>
2026-04-15 10:39:19 +0200

    Test1

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <algorithm>
 #include <exception>
+#include <vector>
 #include "main.h"
 
 
@@ -17,15 +18,32 @@ ParsedInput parse_line(const std::string& input) {              //This is the in
     std::istringstream iss(input);
     ParsedInput result;
 
-    if(!(iss >> result.country >> result.misc >> result.rating)){
+    if(!(iss >> result.country >> result.misc >> result.rating)){           //Check Format
         throw std::invalid_argument ("Invalid Input Format");
     }
+    
+
+    if(result.rating[0]!='A' && result.rating[0]!='B' && result.rating[0]!='C'){    //Check if its A B or C
+        throw std::invalid_argument ("Wrong rating");
+    }
+
+    if(result.rating=="AAA"){
+        if(result.rating.size()>2){
+            throw std::invalid_argument("Cant put Predicate on triple A");     //Check if Triple A is right
+        }
+        
+    }
+
+    return result;
     
 }
 
 
 
 int main(){
+
+    std::vector<std::string> countries;
+    //
 
     
     
